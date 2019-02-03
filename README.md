2406cec1ca0286f590cbd004017641b4dbca3a24
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-03 04:01:36 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Scraping Kickstarter
 
 ## Objectives
--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,9 @@
-# require libraries/modules here
+require 'pry'
+require 'nokogiri'
+
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
